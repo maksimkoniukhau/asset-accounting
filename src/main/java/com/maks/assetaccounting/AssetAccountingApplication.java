b1799d57d03f87e93949de197abeec025ebc42a5
Maksimys87 <maksimys87051@gmail.com>
2018-12-17 20:17:47 +0300

    Initializing application

---
+++ src/main/java/com/maks/assetaccounting/AssetAccountingApplication.java
@@ -0,0 +1,14 @@
+package com.maks.assetaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetAccountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetAccountingApplication.class, args);
+    }
+
+}
+
